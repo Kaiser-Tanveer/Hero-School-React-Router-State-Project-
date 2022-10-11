8dcc13cb09b1a567c63d125f496ac1752def9b16
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-10-11 11:23:14 +0600

    Options added with Quiz

--- src/Components/Quizzes/Opt.js
+++ src/Components/Quizzes/Opt.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
-const Opt = ({option}) => {
+import { EyeIcon } from '@heroicons/react/24/solid';
+const Opt = ({option, correctAnswer}) => {
     return (
         <div className='row'>
-            <p className=''>{option}</p>
+            <p className='w-50 mx-auto text-start'><input type="radio" className='form-check-input" type="radio" value=""'></input> {option}</p>
+            <EyeIcon className='eye-icon'></EyeIcon>
         </div>
     );
 };
