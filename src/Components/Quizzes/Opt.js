4662fec1f506826f23b1a3d30f85bc5bc69b6d54
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-10-11 12:45:02 +0600

    click to see correct ans Committed

--- src/Components/Quizzes/Opt.js
+++ src/Components/Quizzes/Opt.js
@@ -1,7 +1,8 @@
 import React from 'react';
-const Opt = ({option, correctAnswer}) => {
+const Opt = ({option}) => {
+    
     return (
-        <div className='row'>
+        <div>
             <p className='w-50 mx-auto text-start'><input type="radio" className='form-check-input" type="radio" value=""'></input> {option}</p>
         </div>
     );
--- src/Components/Quizzes/Quiz.js
+++ src/Components/Quizzes/Quiz.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Quiz.css'
 import Opt from './Opt';
 import { EyeIcon } from '@heroicons/react/24/solid';
@@ -7,20 +7,29 @@ const Quiz = ({quiz}) => {
     console.log(quiz);
     const {options, question, correctAnswer} = quiz;
     // console.log(options);
+    const [ans, setAns] = useState();
+    const andHandler = (correctAnswer) => {
+        setAns(correctAnswer);
+        return ans;
+    }
     return (
-        <div className='w-75 mx-auto shadow my-5 py-3 rounded'>
-            <h5 className='py-2 bg-primary text-white shadow'>Q: {question}</h5>
-            <p>
+        <div className='w-75 mx-auto shadow my-5 rounded'>
+            <h5 className='py-2  mt-0 bg-primary text-white rounded-top shadow'>Q: {question}</h5>
+            <p className='pt-3'>
                 {
                     options.map((option, idx) => <Opt
                     key={idx}
                     option={option}
+                    ans={ans}
                     correctAnswer={correctAnswer}
                     >
                     </Opt>)
                 }
             </p>
-            <button className='border-0 bg-white'> Check Correct Answer <EyeIcon className='eye-icon'></EyeIcon></button>
+            <div className='d-flex flex-column-reverse flex-md-row justify-content-between p-2 border border-primary bg-light rounded-bottom shadow'>
+                <h5 className='p-4'>Ans: {ans}</h5>
+                <button onClick={()=>andHandler(correctAnswer)} className='border-0 bg-light'> Check Correct Answer <EyeIcon className='eye-icon'></EyeIcon></button>
+            </div>
         </div>
     );
 };
